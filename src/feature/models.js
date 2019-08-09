625bde523b4d46a1d854402acd37301266e718b3
Jaime Noel Alvarez Luna <jalvarez@serlicorera.com>
2019-08-09 07:16:26 -0600

    add eslint

--- src/feature/models.js
+++ src/feature/models.js
@@ -1,27 +1,28 @@
 import Sequelize from 'sequelize';
-import User from './user/user.model'
-import Message from './message/message.model'
 
 const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.DATABASE_USER,
-    process.env.DATABASE_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DATABASE_HOST
-    },
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DATABASE_HOST,
+  },
 );
 
-const models = {
-    User: sequelize.import('User', require('./user/user.model')),
-    Message: sequelize.import('Message', Message)
-};
+const modelNames = ['User', 'Message'];
 
-Object.keys(models).forEach(key => {
-    if ('associate' in models[key]) {
-        models[key].associate(models);
-    }
+const models = modelNames.reduce((acc, model) => {
+  acc[model] = sequelize.import(model, require(`./${model.toLowerCase()}/${model.toLowerCase()}.model`));
+  return acc;
+}, {});
+
+
+Object.keys(models).forEach((key) => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
 });
 
-export {sequelize};
+export { sequelize };
 export default models;
--- src/graphql.js
+++ src/graphql.js
@@ -1,84 +1,84 @@
-import {ApolloServer, AuthenticationError} from 'apollo-server-lambda';
-import models, {sequelize} from './feature/models';
-import schema from './feature/schema'
-import resolvers from './feature/resolvers'
+import { ApolloServer, AuthenticationError } from 'apollo-server-lambda';
 import jwt from 'jsonwebtoken';
+import models, { sequelize } from './feature/models';
+import schema from './feature/schema';
+import resolvers from './feature/resolvers';
 
 
-const getCurrentUser = async req => {
-    console.log({req});
+const getCurrentUser = async (req) => {
+  console.log({ req });
 
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
 
-    if (token) {
-        try {
-            return await jwt.verify(token, process.env.SECRET);
-        } catch (e) {
-            throw new AuthenticationError('Your session expired. Sign in again.')
-        }
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+      throw new AuthenticationError('Your session expired. Sign in again.');
     }
+  }
 };
 
 const createUsersWithMessages = async () => {
-    await models.User.create(
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      email: 'hello@robin.com',
+      password: 'rwieruch',
+      messages: [
         {
-            username: 'rwieruch',
-            email: 'hello@robin.com',
-            password: 'rwieruch',
-            messages: [
-                {
-                    text: 'Published the Road to learn React',
-                },
-            ],
+          text: 'Published the Road to learn React',
         },
-        {
-            include: [models.Message],
-        },
-    );
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
 
-    await models.User.create(
+  await models.User.create(
+    {
+      username: 'ddavids',
+      email: 'hello@david.com',
+      password: 'ddavids',
+      messages: [
         {
-            username: 'ddavids',
-            email: 'hello@david.com',
-            password: 'ddavids',
-            messages: [
-                {
-                    text: 'Happy to release ...',
-                },
-                {
-                    text: 'Published a complete ...',
-                },
-            ],
+          text: 'Happy to release ...',
         },
         {
-            include: [models.Message],
+          text: 'Published a complete ...',
         },
-    );
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
 };
 
 
 sequelize.sync();
 
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
-    formatError: error => {
-        const message = error.message
-            .replace('SequelizeValidationError: ', '')
-            .replace('Validation error: ', '');
-        return {...error, message};
-    },
-    context: async ({event, ...rest}) => {
-        const currentUser = await getCurrentUser(event);
+  typeDefs: schema,
+  resolvers,
+  formatError: (error) => {
+    const message = error.message
+      .replace('SequelizeValidationError: ', '')
+      .replace('Validation error: ', '');
+    return { ...error, message };
+  },
+  context: async ({ event, ...rest }) => {
+    const currentUser = await getCurrentUser(event);
 
-        return ({
-            models,
-            currentUser,
-            secret: process.env.SECRET
-        })
-    },
-    cors: {origin: '*', credentials: true}
+    return ({
+      models,
+      currentUser,
+      secret: process.env.SECRET,
+    });
+  },
+  cors: { origin: '*', credentials: true },
 });
 
 
